10174177efa73c3fa4f606ba0c6f49d6b3603030
Wendy Dherin <wendydherin@gmail.com>
2017-02-28 21:46:03 -0800

    Implement findFirst and isSorted functions.

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -16,6 +16,37 @@ object MyModule {
     else nthFib(n-1) + nthFib(n-2)
   }
 
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= ss.length) -1
+      else if (ss(n) == key) n
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  def findFirst2[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  def lessThan(x: Int, y: Int): Boolean = x < y
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+      if (n == as.length-1) true
+      else if (!ordered(as(n), as(n+1))) false
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
   private def formatAbs(n: Int) = {
     val msg = "The absolute value of %d is %d."
     msg.format(n, abs(n))
@@ -30,6 +61,8 @@ object MyModule {
     val msg = "Item %d in the Fibonacci sequence is %d."
     msg.format(n, nthFib(n))
   }
+
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 }
